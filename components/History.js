3c4e82ebbc8af5464cef84d2c7c46a6663838518
Tharunkumar <tharunkumarsk@gmail.com>
2020-01-06 14:53:59 +0100

    Feat:Design today with button style

--- components/History.js
+++ components/History.js
@@ -1,63 +1,104 @@
-import React ,{Component}from "react";
-import { Text, View } from "react-native";
-import { receiveEntries,addEntry } from '../actions/index';
-import { timeToString,getDailyReminderValue } from '../utils/helpers';
-import { fetchCalendarResults } from '../utils/api';
-import { connect } from 'react-redux';
-import  UdaciFitnessCalendar  from 'udacifitness-calendar';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableOpacity
+} from "react-native";
+import { receiveEntries, addEntry } from "../actions/index";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import { fetchCalendarResults } from "../utils/api";
+import { connect } from "react-redux";
+import UdaciFitnessCalendar from "udacifitness-calendar";
+import { white } from "../utils/colors";
+import DateHeader from "./Dateheader";
 
 class History extends Component {
-    componentDidMount(){
-        const {dispatch} = this.props
-        fetchCalendarResults().then((entries) =>
-        dispatch(receiveEntries(entries)))
-        .then(({entries}) =>{
-            if(!entries[timeToString()]){
-                dispatch(addEntry({
-                    [timeToString()]:getDailyReminderValue()
-                }))
-            }
-        })
-
-
-    }
-    renderItem  =({today,...metrics},formattedDate,key) =>{
-    <View>
-        {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>
+  componentDidMount() {
+    const { dispatch } = this.props;
+    fetchCalendarResults()
+      .then(entries => dispatch(receiveEntries(entries)))
+      .then(({ entries }) => {
+        if (!entries[timeToString()]) {
+          dispatch(
+            addEntry({
+              [timeToString()]: getDailyReminderValue()
+            })
+          );
         }
-    </View>
+      });
+  }
+  renderItem = ({ today, ...metrics }, formattedDate, key) => {
+    {
+      console.log(JSON.stringify(metrics));
+      console.log(today);
+      console.log(formattedDate);
     }
-
-    renderEmptyDate(formattedDate){
-    return(
+    <View style={styles.item}>
+      {today ? (
         <View>
-            <Text>No date for thi date !</Text>
+          <DateHeader date={formattedDate} />
+          <Text style={styles.noDataText}>{today}</Text>
         </View>
-    )
-    }
+      ) : (
+        <TouchableOpacity onPress={() => console.log("Pressed!")}>
+          <Text>{JSON.stringify(metrics)}</Text>
+        </TouchableOpacity>
+      )}
+    </View>;
+  };
 
-    render() {
-        const {entries} = this.props
-        return (
-            <UdaciFitnessCalendar
-            items={entries}
-            renderItem={this.renderItem}
-            renderEmptyDate={this.renderEmptyDate}
-          />
-    //     <View>
-    // <Text>{JSON.stringify(entries)}</Text>
-    // </View>
+  renderEmptyDate(formattedDate) {
+    return (
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
+      </View>
+    );
+  }
 
-        )
-      }
+  render() {
+    const { entries } = this.props;
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
+    );
+  }
 }
 
-function mapStateToProps(entries){
-    return{
-        entries
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === "ios" ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: "center",
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: "rgba(0, 0, 0, 0.24)",
+    shadowOffset: {
+      width: 0,
+      height: 3
     }
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+});
 
+function mapStateToProps(entries) {
+  return {
+    entries
+  };
 }
-export default connect(mapStateToProps)(History) 
+export default connect(mapStateToProps)(History);
