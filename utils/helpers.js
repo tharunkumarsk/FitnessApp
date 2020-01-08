e7edce0217361ca743281053945d8257b83f3f19
Tharunkumar <tharunkumarsk@gmail.com>
2020-01-08 16:09:18 +0100

    Add notification utility

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,6 +6,10 @@ import {
   MaterialCommunityIcons
 } from "@expo/vector-icons";
 import { red, orange, blue, lightPurp, pink, white } from "./colors";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
+
+const NOTIFICATION_KEY = "myapp:notifications";
 
 export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
@@ -156,4 +160,52 @@ export function getMetricMetaInfo(metric) {
     }
   };
   return typeof metric === "undefined" ? info : info[metric];
+}
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+}
+
+function createNotification() {
+  return {
+    title: "Log your stats!",
+    body: "👋 don't forget to log your stats for today!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true
+    }
+  };
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(20);
+            tomorrow.setMinutes(0);
+
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day"
+            });
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
 }
